89df087137e27c4638a3d84f07d29f255b637047
Hyo-Seong <gytjd090792@gmail.com>
2022-01-01 16:27:48 +0900

    Init Solution

---
+++ Src/CoinTradingWithElonMusk/Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CoinTradingWithElonMusk
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
